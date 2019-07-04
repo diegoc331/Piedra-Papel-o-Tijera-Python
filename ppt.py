75b0d9d0edbf34598c5b6a41c78fdfccec082ce4
diegoc331 <diegoc331@gmail.com>
2019-07-03 22:09:33 -0500

    Modificación menú inicio

--- ppt.py
+++ ppt.py
@@ -1,29 +1,34 @@
 from random import choice
 
- 
-print ("Hola, vamos a jugar a piedra, papel o tijera. Elige una de las siguientes opciones: \n 1 = Piedra \n 2 = Papel \n 3 = Tijera \n 4 = Salir " )
+inicio = input('Elige 1 para jugar ó 2 para salir --> ')
 
-Opciones = ['1', '2', '3']
-Maquina = choice((Opciones))
-Player = input( "-->: ")
+if inicio == '1':
+  
+  print ("Hola, vamos a jugar a piedra, papel o tijera. Elige una de las siguientes opciones: \n 1 = Piedra \n 2 = Papel \n 3 = Tijera \n 4 = Salir " )
 
-if Player == Maquina:
-   print("Empate")
+  Opciones = ['1', '2', '3']
+  Maquina = choice((Opciones))
+  Player = input( "-->: ")
 
-if (Player == '1'):
-  if (Maquina == '2'):
-    print("Perdiste")
-  elif (Maquina == '3'):
-     print("Ganaste")
+  if Player == Maquina:
+    print("Empate")
 
-if (Player == '2'):
-  if (Maquina == '3'):
-     print("Perdiste")
-  elif (Maquina == '1'):
+  if (Player == '1'):
+    if (Maquina == '2'):
+      print("Perdiste")
+    elif (Maquina == '3'):
       print("Ganaste")
 
-if (Player == '3'):
-  if (Maquina == '1'):
-    print("Perdiste")
-  elif (Maquina == '2'):
-    print("Ganaste")
+  if (Player == '2'):
+    if (Maquina == '3'):
+      print("Perdiste")
+    elif (Maquina == '1'):
+        print("Ganaste")
+
+  if (Player == '3'):
+    if (Maquina == '1'):
+      print("Perdiste")
+    elif (Maquina == '2'):
+      print("Ganaste")
+else:
+  print ('Bye!')
